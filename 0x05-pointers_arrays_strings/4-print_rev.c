e8adaaecfe723b5d3a18dc3e14704f323ab3a8ed
jjenus <biz.jenus@gmail.com>
2022-06-16 15:46:20 +0100

    added 5-rev_string.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,12 @@ void print_rev(char *s)
 	int count = 0;
 
 	while (*(s + count) != '\0')
+	{
 		count++;
-	count--;
+	}
 	for (count; count >= 0; count--)
+	{
 		_putchar(s[count]);
+	}
 	_putchar('\n');
 }
