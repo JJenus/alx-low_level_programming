36e19f0de867f21adce62210a4d23c2407540533
jjenus <biz.jenus@gmail.com>
2022-06-20 17:48:12 +0100

    added 3-strspn.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,18 +4,15 @@
  * _memcpy - copies memory area,
  * @dest: destination memory area.
  * @src: source memory area.
- * @n: number of bytes filled.
+ * @n: bytes filled.
  * Return: the pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		*(dest + i) =  *(src + i);
-		i++;
-	}
 
 	return (dest);
 }
