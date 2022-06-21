f391cda122c72ce4fe02059e1188ec58646b6913
jjenus <biz.jenus@gmail.com>
2022-06-21 08:41:37 +0100

    updated 2-strchr.c to be betty compliant

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,17 +7,19 @@
  * @c: character.
  * Return: the pointer to the first occurrence of the character c.
  */
-char *_strchr(char *s, char c){
-    int i = 0;
-    char *ptr = NULL;
-    
-    while (*(s + i) != '\0'){
-        if(*(s + i) == c){
-            ptr = &*(s + i);
-            break;
-        }
-        i++;
-    }
-    
-    return (ptr);
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *ptr = NULL;
+	while (*(s + i) != '\0')
+	{
+		if(*(s + i) == c)
+		{
+			ptr = &*(s + i);
+			break;
+		}
+		i++;
+	}
+
+	return (ptr);
 }
